8946916b1f5e1dfbe038b542582b66bdc3a5a515
itopnaibaho123 <christopher.naibaho14@gmail.com>
2025-12-11 18:39:12 +0700

    update on ktr file and output

---
+++ Data/dim_status_order.sql
@@ -0,0 +1,7 @@
+INSERT INTO dwh_gravity.dim_status_order (status_id,status_value) VALUES
+	 ('1','Order Received'),
+	 ('2','Pending Delivery'),
+	 ('3','Delivery In Progress'),
+	 ('4','Delivered'),
+	 ('5','Cancelled'),
+	 ('6','Returned');
